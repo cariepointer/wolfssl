4179dbff91f08d0813e5d3fc39d2fa2c0decb09c
Carie Pointer <carie@wolfssl.com>
2019-10-30 15:28:13 -0700

    Updates for X509_STORE

--- src/ssl.c
+++ src/ssl.c
@@ -22360,7 +22360,7 @@ int wolfSSL_i2d_X509(WOLFSSL_X509* x509, unsigned char** out)
     const unsigned char* der;
     int derSz = 0;
 
-    if (x509 == NULL || out == NULL) {
+    if (x509 == NULL) {
         return BAD_FUNC_ARG;
     }
 
@@ -22369,14 +22369,15 @@ int wolfSSL_i2d_X509(WOLFSSL_X509* x509, unsigned char** out)
         return MEMORY_E;
     }
 
-    if (*out == NULL) {
+    if (out != NULL && *out == NULL) {
         *out = (unsigned char*)XMALLOC(derSz, NULL, DYNAMIC_TYPE_OPENSSL);
         if (*out == NULL) {
             return MEMORY_E;
         }
     }
 
-    XMEMCPY(*out, der, derSz);
+    if (out != NULL)
+        XMEMCPY(*out, der, derSz);
 
     return derSz;
 }
@@ -23123,6 +23124,9 @@ int wolfSSL_X509_STORE_add_cert(WOLFSSL_X509_STORE* store, WOLFSSL_X509* x509)
 
 WOLFSSL_X509_STORE* wolfSSL_X509_STORE_new(void)
 {
+#if defined(WOLFSSL_QT) || defined(OPENSSL_ALL)
+    WOLFSSL_STACK* sk;
+#endif
     WOLFSSL_X509_STORE* store = NULL;
     WOLFSSL_ENTER("SSL_X509_STORE_new");
 
@@ -23143,6 +23147,15 @@ WOLFSSL_X509_STORE* wolfSSL_X509_STORE_new(void)
         goto err_exit;
     if (InitCRL(store->crl, NULL) < 0)
         goto err_exit;
+#endif
+
+#if defined(WOLFSSL_QT) || defined(OPENSSL_ALL)
+    sk = wolfSSL_sk_new_null();
+    if (sk == NULL) {
+        WOLFSSL_MSG("WOLFSSL_STACK memory error");
+        goto err_exit;
+    }
+    store->ex_data.data = sk;
 #endif
 
 #ifdef OPENSSL_EXTRA
@@ -23177,6 +23190,10 @@ void wolfSSL_X509_STORE_free(WOLFSSL_X509_STORE* store)
 #ifdef OPENSSL_EXTRA
         if (store->param != NULL)
             XFREE(store->param, NULL, DYNAMIC_TYPE_OPENSSL);
+#endif
+#if defined(WOLFSSL_QT) || defined(OPENSSL_ALL)
+        if (store->ex_data.data != NULL)
+            wolfSSL_sk_GENERIC_free(store->ex_data.data);
 #endif
         XFREE(store, NULL, DYNAMIC_TYPE_X509_STORE);
     }
