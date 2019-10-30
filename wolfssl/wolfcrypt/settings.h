e27144a42e7e8b5cafa00a935c6861c6902ce605
Carie Pointer <carie@wolfssl.com>
2019-10-30 09:14:02 -0700

    Add opensslv.h and wolfcrypt/settings.h changes

--- wolfssl/wolfcrypt/settings.h
+++ wolfssl/wolfcrypt/settings.h
@@ -1974,7 +1974,7 @@ extern void uITRON4_free(void *p) ;
     #endif
 #endif
 
-#if defined(WOLFSSL_NGINX)
+#if defined(WOLFSSL_NGINX) || defined(WOLFSSL_QT) || defined(OPENSSL_ALL)
     #define SSL_CTRL_SET_TLSEXT_HOSTNAME
 #endif
 
